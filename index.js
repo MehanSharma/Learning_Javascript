94ec4bfd1c8c6ba09b245676173e8d06614b7887
MehanSharma <mehansharma.ms@gmail.com>
2024-08-02 12:24:55 +0000

    2-8-24

---
+++ index.js
@@ -0,0 +1,5 @@
+// console.log("hello")
+
+let a=null
+
+console.log(a);
---
+++ operators.js
@@ -0,0 +1,30 @@
+// Arithmetic Operators 
+// +,-,*,/,%
+// console.log(10+10);
+// console.log(100-10);
+// console.log(100*10);
+// console.log(100/10);
+// console.log(100%10);
+
+// Relational / Comparison 
+// <,>,<=,>=,==,!=
+
+// console.log(10<5);
+// console.log(10>5);
+// console.log(10>=100);
+// console.log(1000<=100);
+
+// console.log(10!=100);
+
+console.log(10-10);
+console.log(10+10);
+console.log(10*10);
+console.log(10/10);
+console.log(10%10);
+
+console.log(10>5);
+console.log(10<5);
+console.log(10<=10);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+console.log(10>=100);
+console.log(10!=100); 
+console.log(10==100);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
